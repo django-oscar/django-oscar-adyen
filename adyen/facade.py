ae3f1470a1ac72e12c5147442d7e26bdbcc1e242
Mathieu Richardoz <metamatik@gmail.com>
2014-09-23 11:34:33 +0200

    Allow custom IP address HTTP header name

--- adyen/facade.py
+++ adyen/facade.py
@@ -72,24 +72,24 @@ class Facade():
         else:
             status = Constants.PAYMENT_RESULT_REFUSED
 
+        # We need to fetch the real origin IP address. According to
+        # the platform architecture, it may be transmitted to our application
+        # via vastly variable HTTP headers. The name of the relevant header is
+        # therefore configurable via the ADYEN_IP_ADDRESS_HTTP_HEADER Django
+        # setting. We fallback on the basic `REMOTE_ADDR`one which is the
+        # standard, unproxied, HTTP one.
         try:
+            ip_address_http_header = settings.ADYEN_IP_ADDRESS_HTTP_HEADER
+        except AttributeError:
+            ip_address_http_header = 'REMOTE_ADDR'
 
-            # We are behind a reverse proxy, so the "real" IP
-            # address has been forwarded in the X-Forwarded-For
-            # HTTP header -- See:
-            # http://www.micahcarrick.com/django-ip-address-behind-nginx-proxy.html
-
-            ip_address = request.META['X_HTTP_FORWARDED_FOR']
-
+        # We can now fetch the IP address...
+        try:
+            ip_address = request.META[ip_address_http_header]
         except KeyError:
+            ip_address = None
 
-            # Fallback on the classic Remote-Addr HTTP
-            # header for non-proxied environments, eg.
-            # those using [dev|grunt]server.
-
-            ip_address = request.META['REMOTE_ADDR']
-
-        # record audit trail
+        # ... and record the audit trail.
         AdyenTransaction.objects.create(
             order_number=order_number,
             reference=reference,
@@ -103,7 +103,7 @@ class Facade():
             feedback_message = self.FEEDBACK_MESSAGES.get(feedback_code)
             raise UnableToTakePayment(feedback_message)
 
-        # normalize output data
+        # We now normalize the output data to feed it back to the Oscar shop.
         output_data = {
             'method': 'adyen',
             'amount': amount,
