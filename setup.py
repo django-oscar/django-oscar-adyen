fb2840660fb6562d02e0fc46222db0758b56cbc1
Mathieu Richardoz <metamatik@gmail.com>
2014-10-21 09:55:27 +0200

    Bump version number to 0.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.environ.get('USER', '') == 'vagrant':
 setup(
 
     name='django-oscar-adyen',
-    version='0.2.0-feature-payment-postback',
+    version='0.2.0',
     url='https://github.com/oscaro/django-oscar-adyen',
     author='Mathieu Richardoz',
     author_email='mr@babik.fr',
