82a694b29755b6e723a43e4b38a03e5b2f5e3f47
Mathieu Richardoz <metamatik@gmail.com>
2015-01-21 17:27:09 +0100

    Bump version number to 0.2.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.environ.get('USER', '') == 'vagrant':
 setup(
 
     name='django-oscar-adyen',
-    version='0.2.4',
+    version='0.2.5',
     url='https://github.com/oscaro/django-oscar-adyen',
     author='Mathieu Richardoz',
     author_email='mr@babik.fr',
