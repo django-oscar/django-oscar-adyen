99ae0b5a47e4f155ea33256e8d88de111a2ce752
Alexander Gaevsky <sasha@sasha0.ru>
2019-05-06 18:17:49 +0300

    Rework test settings.

--- tests/settings.py
+++ tests/settings.py
@@ -4,6 +4,9 @@
 
 import os
 
+from oscar import get_core_apps
+from oscar.defaults import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = '+&l^d!%soa4gxsnx7_txbo0x3uv$@4i&n!r8yte72otwqo7vmh'
@@ -17,19 +20,32 @@ DATABASES = {
 
 OSCAR_DEFAULT_CURRENCY = 'EUR'
 OSCAR_REQUIRED_ADDRESS_FIELDS = []
+OSCAR_SLUG_ALLOW_UNICODE = False
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
 )
 
-INSTALLED_APPS = (
-    'adyen',
-    'django.contrib.contenttypes',
+INSTALLED_APPS = [
     'django.contrib.auth',
-)
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    'django.contrib.flatpages',
 
-OSCAR_SLUG_ALLOW_UNICODE = False
+    'adyen',
+] + get_core_apps()
+
 ADYEN_IDENTIFIER = 'OscaroFR'
 ADYEN_SECRET_KEY = 'oscaroscaroscaro'
 ADYEN_ACTION_URL = 'https://test.adyen.com/hpp/select.shtml'
 ADYEN_SKIN_CODE = 'cqQJKZpg'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+    },
+}
