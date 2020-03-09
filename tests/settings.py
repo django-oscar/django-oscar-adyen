9b216fcf9e8ec3567300bc0d10a16fca646a6244
Alexander Gaevsky <sasha@sasha0.ru>
2020-03-09 11:44:20 +0200

    Actualize test settings to be compatible with Oscar 2.0.

--- tests/settings.py
+++ tests/settings.py
@@ -4,7 +4,6 @@
 
 import os
 
-from oscar import get_core_apps
 from oscar.defaults import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -27,17 +26,48 @@ MIDDLEWARE_CLASSES = (
 )
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.admin',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
     'django.contrib.flatpages',
 
+    'oscar',
+    'oscar.apps.analytics',
+    'oscar.apps.checkout',
+    'oscar.apps.address',
+    'oscar.apps.shipping',
+    'oscar.apps.catalogue',
+    'oscar.apps.catalogue.reviews',
+    'oscar.apps.partner',
+    'oscar.apps.basket',
+    'oscar.apps.payment',
+    'oscar.apps.offer',
+    'oscar.apps.order',
+    'oscar.apps.customer',
+    'oscar.apps.search',
+    'oscar.apps.voucher',
+    'oscar.apps.wishlists',
+    'oscar.apps.dashboard',
+    'oscar.apps.dashboard.reports',
+    'oscar.apps.dashboard.users',
+    'oscar.apps.dashboard.orders',
+    'oscar.apps.dashboard.catalogue',
+    'oscar.apps.dashboard.offers',
+    'oscar.apps.dashboard.partners',
+    'oscar.apps.dashboard.pages',
+    'oscar.apps.dashboard.ranges',
+    'oscar.apps.dashboard.reviews',
+    'oscar.apps.dashboard.vouchers',
+    'oscar.apps.dashboard.communications',
+    'oscar.apps.dashboard.shipping',
+
     'adyen',
-] + get_core_apps()
+]
 
 ADYEN_IDENTIFIER = 'OscaroFR'
 ADYEN_SECRET_KEY = 'oscaroscaroscaro'
